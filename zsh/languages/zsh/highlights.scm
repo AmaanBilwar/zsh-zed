48cb78688d17a9504124fa645405a9d9e877fea5
Amaan Bilwar <bilwarad@mail.uc.edu>
2026-04-04 19:49:16 +0530

    refine zsh highlight and outline queries

--- zsh/languages/zsh/highlights.scm
+++ zsh/languages/zsh/highlights.scm
@@ -1,17 +1,33 @@
 [
   (string)
   (raw_string)
+  (ansi_c_string)
+  (translated_string)
   (heredoc_body)
   (heredoc_start)
 ] @string
 
 [
-    (command_name)
-    (declaration_command)
+  (command_name)
 ] @function
 
+(declaration_command) @keyword
 
-(variable_name) @property
+
+(variable_name) @variable
+
+[
+  (simple_expansion)
+] @variable
+
+(
+  (concatenation
+    (simple_expansion) @variable)
+)
+
+(special_variable_name) @variable.special
+
+(number) @number
 
 [
   "case"
@@ -46,6 +62,7 @@
 ] @embedded
 
 [
+  "$"
   "&&"
   ">"
   ">>"
@@ -54,6 +71,21 @@
 ] @operator
 
 (
-  (command (_) @constant)
-  (#match? @constant "^-")
+  (flag) @operator
+)
+
+(
+  (command (_) @operator)
+  (#match? @operator "^-")
+)
+
+(
+  (word) @string.special
+  (#match? @string.special "^\\*\\.[A-Za-z0-9_]+\\([A-Za-z0-9_,-]+\\)$")
+)
+
+(
+  (concatenation
+    (string)
+    (word) @string.special)
 )
